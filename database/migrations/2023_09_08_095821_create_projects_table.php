6d193add749f71d72ea5b4b9521934795a240bed
Sian <simple0504@hotmail.com>
2023-09-21 20:09:59 +0800

    【EDIT】 Update the project migration notes.

--- database/migrations/2023_09_08_095821_create_projects_table.php
+++ database/migrations/2023_09_08_095821_create_projects_table.php
@@ -20,8 +20,8 @@ class CreateProjectsTable extends Migration
             $table->longText('description')->nullable()->comment('專案描述');
             $table->integer('staff_id')->unsigned()->index()->comment('專案負責人ID');
             $table->integer('sort')->default(0)->comment('專案優先順序');
-            $table->date('start_date')->nullable()->comment('專案開始時間');
-            $table->date('end_date')->nullable()->comment('專案完成時間');
+            $table->date('start_date')->nullable()->comment('專案開始日');
+            $table->date('end_date')->nullable()->comment('專案完成日');
             $table->integer('status')->default(0)->comment('專案狀態【0=尚未進行、1=進行中、2=驗收中、3=收款中、4=結案】');
             $table->text('remark')->nullable()->comment('備註');
             $table->timestamps();
